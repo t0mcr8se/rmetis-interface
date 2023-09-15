eeaec5d0f65bcf450a484a9bfe306b9654cf605b
Rami Husami <husamirami@gmail.com>
2023-09-15 21:37:35 +0300

    change warning banner style

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -19,7 +19,7 @@ export default function Welcome() {
       </div>
       <div className="absolute w-[1360px] h-[270px] top-[480px] left-[280px] bg-[#8755cd4c] rounded-[40px] overflow-hidden">
         <div className="absolute w-[820px] h-[153px] top-[20px] left-[336px]">
-          <p className="absolute w-[820px] h-[86px] top-[67px] left-0 [font-family:'Inter-Regular',Helvetica] font-normal text-white text-[24px] tracking-[0] leading-[34px]">
+          <p className="absolute w-[820px] h-[86px] top-[50px] left-0 [font-family:'Inter-Regular',Helvetica] font-normal text-white text-[24px] tracking-[0] leading-[34px]">
             <span className="[font-family:'Inter-Regular',Helvetica] font-normal text-white text-[24px] tracking-[0] leading-[34px]">
               Please make sure you are visiting{" "}
             </span>
@@ -29,21 +29,23 @@ export default function Welcome() {
             <span className="[font-family:'Inter-Regular',Helvetica] font-normal text-white text-[24px] tracking-[0] leading-[34px]">
               {" "}
               <br />
-              Check the URL carefully. Read the{" "}
+              Check the URL carefully.{' '} <br/>
             </span>
-            <span className="[font-family:'Inter-Bold',Helvetica] font-bold">
-              Tutorial
+            {/* <span className="[font-family:'Inter-Regular',Helvetica] font-normal text-red-400 text-[24px] tracking-[0] leading-[34px]">
+            Read the {' '}
+            </span> */}
+            <span className="[font-family:'Inter-Bold',Helvetica] text-red-400 font-bold">
+            Read the TUTORIAL carefully before proceeding!
             </span>
-            <span className="[font-family:'Inter-Regular',Helvetica] font-normal text-white text-[24px] tracking-[0] leading-[34px]">
-              {" "}
-              carefully before proceeding
-            </span>
+            {/* <span className="[font-family:'Inter-Regular',Helvetica] font-normal text-red-400 text-[24px] tracking-[0] leading-[34px]">
+            {' '}carefully before proceeding
+            </span> */}
           </p>
-          <div className="absolute w-[728px] h-[86px] top-0 left-0 [font-family:'Inter-Bold',Helvetica] font-bold text-white text-[44px] tracking-[0] leading-[28px]">
+          <div className="absolute w-[728px] h-[86px] top-0 left-0 [font-family:'Inter-Bold',Helvetica] font-bold text-red-400 text-[44px] tracking-[0] leading-[28px]">
             PHISHING WARNING:
           </div>
         </div>
-        <div className="absolute w-[235px] h-[60px] top-[188px] left-[336px] bg-white rounded-[50px]">
+        <div className="absolute w-[235px] h-[60px] top-[188px] left-[336px] text-red-400 bg-white rounded-[50px]">
           <button onClick={() => window.open("https://metisdao.medium.com/step-by-step-instructions-on-how-to-redeem-your-metis-assets-lost-from-polynetwork-hack-ee57fac03502", "_blank")} className="absolute w-[239px] h-[60px] -top-px -left-px [font-family:'Raleway-Medium',Helvetica] font-medium text-black text-[20px] text-center tracking-[0.20px] leading-[normal]">
             Tutorial Here
           </button>
