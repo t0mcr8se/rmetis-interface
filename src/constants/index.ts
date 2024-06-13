f2e4ed56c383fe44c04e93862e2056619106a21c
Rami Husami <husamirami@gmail.com>
2024-06-13 11:11:07 +0300

    lp compensation

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,24 @@
-import {metis, metisGoerli} from 'wagmi/chains'
+import { defineChain } from 'viem'
+import {Chain, metis} from 'wagmi/chains'
 
-export const SUPPORTED_CHAINS = [metis, metisGoerli]
+export const metisSepolia: Chain = defineChain({
+    id: 59902,
+    name: 'Metis Sepolia',
+    nativeCurrency: { name: 'Metis', symbol: 'tMETIS', decimals: 18 },
+    rpcUrls: {
+        public: { http: ['https://sepolia.metisdevops.link/'] },
+        default: { http: ['https://sepolia.metisdevops.link/'] },
+    },
+    blockExplorers: {
+        default: { name: 'Etherscan', url: 'https://etherscan.io' },
+    },
+    network: "metissepolia"
+
+})
+export const SUPPORTED_CHAINS = [metis, metisSepolia]
 
 export const WALLET_CONNECT_PROJECT_ID = 'da1c3b9d03ca92585b6b7cd31e9e9b6b'
+
 
 export {default as RMETIS_ABI} from '../abi/erc20.json'
 export {default as VESTING_ABI} from '../abi/vesting-vault.json'
@@ -10,18 +26,18 @@ export {default as VESTING_ABI} from '../abi/vesting-vault.json'
 type AddressMap = {[chainId: number]: string}
 
 export const RMETIS_ADDRESSES: AddressMap = {
-    1088: '0x18ebC943Ba08e5FF31832F53C3B011db74Bc1911',
-    599: '0x91Bfc6358A58Df7Fc1A0Ca9D1B1B3B7f08B80986',
+    1088: '0xe08D8c6AF9F0221068E02DfD03A9B421f426332c',
+    59902: '0x4E1139C6AF127D6b346Ea453421d2A9a7Ab4E959',
 }
 
 export const VESTING_ADDRESSES: AddressMap = {
-    1088: '0x4D4F5FAf217143eC1047d944d2e4928e3dCCC9FB',
-    599: '0x4381F671Db28109cAcd31907F3EF5594350A027d',
+    1088: '0x1b28B90a958a4070bfB301929B99C7aEbDF074E2',
+    59902: '0x8eb422ded890B29679893B20db2255B2e9044719',
 }
 
 export const MERKLE_TREES: AddressMap = {
-    1088: "https://raw.githubusercontent.com/t0mcr8se/rmetis-merkle-tree/main/merkle-bsc-0xe552Fb52a4F19e44ef5A967632DBc320B0820639-11170743-29591664-no-contracts.json",
-    599: "https://raw.githubusercontent.com/t0mcr8se/rmetis-merkle-tree/main/merkle-bsc-0xe552Fb52a4F19e44ef5A967632DBc320B0820639-11170743-29591664-no-contracts.json"
+    1088: "https://raw.githubusercontent.com/t0mcr8se/rmetis-merkle-tree/main/merkle-lp-snapshot.json",
+    59902: "https://ipfs.io/ipfs/QmQd4WymhSbQqn2rAeQqKTxjuqicZEDqgEAfj6RW7tF5ac?filename=merkle-lp-snapshot.json",
 }
 
 export const PRECISION = BigInt(10000)
